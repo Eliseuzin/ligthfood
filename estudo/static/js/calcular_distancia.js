ca13213fb1eedd0bb3dc7e5554e708b6fae4193b
Eliseu <eliseumatos321@gmail.com>
2026-06-30 10:07:41 -0300

    fazendo busca de resposta do id=rua

--- estudo/static/js/calcular_distancia.js
+++ estudo/static/js/calcular_distancia.js
@@ -1,6 +1,7 @@
-async function calcular_distanci(){
-    console.log('Funcao chamada')
-    const endereco_rua_cliente = document.getElementById('rua').value
+async function calculardistancia(){
+    console.log('Funcao chamada');
+    console.log(document.getElementById("cep"));
+    const endereco_rua_cliente = document.getElementById("rua").value;
     const resposta = await fetch ("/calcular_distancia",{
         method: "POST",
         headers:{"Content-Type": "application/json"},
@@ -8,9 +9,10 @@ async function calcular_distanci(){
             endereco_rua_cliente:endereco_rua_cliente
         })
     });
+    console.log('resposta recebida');
 
     const data = await resposta.json();
-    const resultado = document.getElementById('resultado')
+    const resultado = document.getElementById('resultado');
 
     if (resposta.ok){
         resultado.innerText=`Distancia: ${data.distancia}, Tempo estimado: ${data.duracao}`;

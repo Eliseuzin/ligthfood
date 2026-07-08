9fbad76d644ebd144b171394701e595c1f1c2aac
Eliseu <eliseumatos321@gmail.com>
2026-07-08 10:51:09 -0300

    fiz a alteracao do ideia correta, origin= enderco_cliente and destinations= endereco_loja

--- estudo/funcao_distancia.py
+++ estudo/funcao_distancia.py
@@ -14,8 +14,8 @@ def calcular_distancia(endereco_cliente):
     # print(request.json)
     url= "https://maps.googleapis.com/maps/api/distancematrix/json"
     params={
-            "origins": endereco_cliente,
-            "destinations": ENDERECO_LOJA_HAMBURGUERIA,
+            "origins": ENDERECO_LOJA_HAMBURGUERIA,
+            "destinations":endereco_cliente,
             "key": GOOGLE_MAPS_API_KEY,
             "language": "pt-BR",
             "units": "metric"
@@ -24,8 +24,10 @@ def calcular_distancia(endereco_cliente):
     response = requests.get(url, params=params)
     data=  response.json()
     # print(data) isso vai mostrar no terminal a resposta da API
-
-
+    #isto é essencial, pois mostra se realmente estamos buscando o endereço pretendido
+    # print("Origem:", ENDERECO_LOJA_HAMBURGUERIA)
+    # print("Destino:", endereco_cliente)
+    
     try:
         distancia= data['rows'][0]['elements'][0]['distance']['text']
         distancia_metros=data['rows'][0]['elements'][0]['distance']['value']
@@ -34,24 +36,3 @@ def calcular_distancia(endereco_cliente):
     except:
         return jsonify({"erro": "Não foi possível calcular a distãncia."
                         }),400
-
-
-# def calcular_frete(distancia_metros):
-#     if distancia_metros <= 2000:
-#         return 6.00
-#     elif distancia_metros <= 3000:
-#         return 7.00
-#     elif distancia_metros <= 4000:
-#         return 8.00
-#     elif distancia_metros <= 5000:
-#         return 9.00
-#     elif distancia_metros <= 6000:
-#         return 10.00
-#     elif distancia_metros <= 7000:
-#         return 11.00
- 
-#     excedente = distancia_metros - 7000
-
-#     return round(10 + (excedente * 0.0016), 2)
-
-# o próximo passo era colocar estado e cidade do cliente, pois para calcular_frete com precisão

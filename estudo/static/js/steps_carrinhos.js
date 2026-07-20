2517dcc4572bac097ee7eeb3c1b52a904cb46a45
Eliseu <eliseumatos321@gmail.com>
2026-07-20 10:42:49 -0300

    removi todos os if(event.target....)

--- estudo/static/js/steps_carrinhos.js
+++ estudo/static/js/steps_carrinhos.js
@@ -1,51 +1,62 @@
 //inicio eventos para abrir/fechar carrinho
 const dentrodocarrinho = document.getElementById("dentrodocarrinho");
 const FecharStep1 = document.getElementById("fecharstep1");
+// document.querySelector(".ativo").classList.remove("ativo");
 
 Meucarrinho.addEventListener("click", () => {
   dentrodocarrinho.style.display = "block";
 });
 
-dentrodocarrinho.addEventListener("click", (event) => {
-  if (event.target === dentrodocarrinho || event.target === FecharStep1) {
-    dentrodocarrinho.style.display = "none";
-  }
-});
+// Inicio fechar carrinho com click fora dele
+dentrodocarrinho.addEventListener("click", (event) =>{
+  dentrodocarrinho.style.display = "none";
+})
+// fim fechar carrinho com click fora dele
+
+// dentrodocarrinho.addEventListener("click", (event) => {
+//   if (event.target === dentrodocarrinho || event.target === FecharStep1) {
+//     dentrodocarrinho.style.display = "none";
+//   }
+// });
+FecharStep1.addEventListener("click", (event)=>{
+  dentrodocarrinho.style.display = "none"
+})
+
 // fim eventos para abrir/fechar carrinho
 
 // inicio continuar passo 1 para o passso 2
 const ContinuarStep1 = document.getElementById("continuar-step1");
 const StepCarrinho1 = document.getElementById("step-carrinho");
-
-
-dentrodocarrinho.addEventListener("click", (event) =>{
-  if (event.target === dentrodocarrinho || event.target === ContinuarStep1){
-    StepCarrinho1.style.display = "none";
-  }
-})
-//  fim continuar passo 1 para o passso 2
-
-//inicio mostrar passso 2
 const StepEntrega1 = document.getElementById("step-entrega");
 
 
-dentrodocarrinho.addEventListener("click", (event) =>{
-  if (event.target === dentrodocarrinho || event.target === ContinuarStep1){
-    StepEntrega1.style.display = "block";
-  }
-})
-// fim mostrar passo 2  
+// dentrodocarrinho.addEventListener("click", (event) =>{
+//   if (event.target === dentrodocarrinho || event.target === ContinuarStep1){
+//     StepCarrinho1.style.display = "none";
+//   }
+// })
+
+ContinuarStep1.addEventListener("click", (event) =>{
+  StepCarrinho1.style.display = "none";
+   //inicio mostrar passso 2
+  //  StepEntrega1.classList.add(".ativo");
+
+  StepEntrega1.style.display = "block";
+   // fim mostrar passo 2  
+
+
+});
+
+//  fim continuar passo 1 para o passso 2
+
 
 // inicio voltar step2 para step1
 const VoltarStep2 = document.getElementById("voltar-step2");
 
-
-dentrodocarrinho.addEventListener("click", (event) =>{
-  if (event.target === dentrodocarrinho || event.target === VoltarStep2){
+VoltarStep2.addEventListener("click", (event) =>{
   StepEntrega1.style.display = "none";
   StepCarrinho1.style.display = "block";
-}
-})
+});
 // fim voltar step2 para step1
 
 // inicio continuar step2 para step3
@@ -53,23 +64,19 @@ const ContinuarStep2 = document.getElementById("continuar-step2");
 const ResumoStep3 = document.getElementById("step-resumo");
 
 
-dentrodocarrinho.addEventListener("click", (event) =>{
-  if(event.target === dentrodocarrinho || event.target === ContinuarStep2){
-    StepEntrega1.style.display = "none";
-    ResumoStep3.style.display = "block";
-  }
-})
+ContinuarStep2.addEventListener("click", (event) =>{
+  StepEntrega1.style.display = "none";
+  ResumoStep3.style.display = "block";
+});
 // fim continuar step2 para step3
 
 // inicio voltar step3 para step2
 const VoltarStep3 = document.getElementById("voltar-step3");
 
 
-dentrodocarrinho.addEventListener("click", (event) =>{
-  if(event.target === dentrodocarrinho || event.target === VoltarStep3){
-    ResumoStep3.style.display = "none";
-    StepEntrega1.style.display = "block";
-  }
+VoltarStep3.addEventListener("click", (event) =>{
+  ResumoStep3.style.display = "none";
+  StepEntrega1.style.display = "block"
 })
 // fim voltar step3 para step2
 
